66315ef60c5e62394eb02f5ad9c2a8afd5568acf
Santiago Desktop <santiagoclv@gmail.com>
2018-09-08 18:49:46 -0300

    end of course

--- 4-auth-graph/server/server.js
+++ 4-auth-graph/server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = "mongodb://santiago:santiago1989@ds141952.mlab.com:41952/graph-auth";
+const MONGO_URI = "mongodb://mongiurl";
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
